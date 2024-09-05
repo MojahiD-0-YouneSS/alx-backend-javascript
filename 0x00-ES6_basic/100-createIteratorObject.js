b3648bd84e9053bc8f245ebd0c2d80fa9dd459cd
MOJAHID YOUNESS <mojahidyouness0@gmail.com>
2024-09-05 11:55:45 +0000

    js 3

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+
+  for (const department in report.allEmployees) {
+    if (report.allEmployees.hasOwnProperty(department)) {
+      allEmployees = allEmployees.concat(report.allEmployees[department]);
+    }
+  }
+
+  return allEmployees[Symbol.iterator]();
+}
+
